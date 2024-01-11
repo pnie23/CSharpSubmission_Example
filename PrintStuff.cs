d9877e90cd4eb7700f2d2e8a8efcccab24e9fa39
pnie23 <150399640+pnie23@users.noreply.github.com>
2024-01-11 12:28:46 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpData
+{
+    internal class PrintStuff
+    {
+        private string language;
+        //Constructor
+        //Overloading method is below
+        public PrintStuff ()
+        {
+            language = "EN";
+        }
+        public PrintStuff(string temp)
+        {
+            language = temp;
+        }
+        public void PrintName(string firstName)
+        {
+            if (language == "EN")
+            {
+            System.Console.WriteLine("Hello, " + firstName + "!");
+            }
+            if (language == "PT")
+            {
+                System.Console.WriteLine("Oi, " + firstName + "!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using MoreCSharpData;
+using System.Xml.Linq;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff("PT");
+
+        string name = "";
+
+        System.Console.WriteLine("Please enter your name:");
+        name = System.Console.ReadLine();
+
+        ps.PrintName(name);
+    }
+}
